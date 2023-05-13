345dd70defbd8b6e15f4a6dbbe5283b4ee6764f8
Vicky John <vicky.d.john@email.com>
2023-05-12 22:10:22 -0700

    added conditionals for edge conditions

--- timer1.js
+++ timer1.js
@@ -1,10 +1,20 @@
 const alarms = process.argv.slice(2);
 let alarmsx1000 = [];
-for (let i = 0; i < alarms.length; i++) {
-  alarmsx1000.push(alarms[i]*1000);
-}
-for (alarm of alarmsx1000) {
-  setTimeout(() => {
-    process.stdout.write('.');
-  }, alarm);
+if (alarms.length === 0) {
+  return;
+} else {
+  for (let i = 0; i < alarms.length; i++) {
+    alarmsx1000.push(alarms[i] * 1000);
+  }
+  for (alarm of alarmsx1000) {
+    if (alarm < 1) {
+      continue;
+    } else if (isNaN(alarm)) {
+      continue;
+    } else {
+      setTimeout(() => {
+        process.stdout.write('.\n');
+      }, alarm);
+    }
+  }
 }
