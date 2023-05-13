a566e8eba4f3c8851f9a0b2f43f7ceee5ccc9d92
Vicky John <vicky.d.john@email.com>
2023-05-12 22:41:51 -0700

    Refactored code to filer out unnecessary elements and use forEach instead of for...of to simplify code

--- timer1.js
+++ timer1.js
@@ -1,20 +1,8 @@
 const alarms = process.argv.slice(2);
-let alarmsx1000 = [];
-if (alarms.length === 0) {
-  return;
-} else {
-  for (let i = 0; i < alarms.length; i++) {
-    alarmsx1000.push(alarms[i] * 1000);
-  }
-  for (alarm of alarmsx1000) {
-    if (alarm < 1) {
-      continue;
-    } else if (isNaN(alarm)) {
-      continue;
-    } else {
-      setTimeout(() => {
-        process.stdout.write('.\n');
-      }, alarm);
-    }
-  }
-}
+alarms
+  .filter(alarm => !isNaN(alarm) && alarm >= 1)
+  .forEach(alarm => {
+    setTimeout(() => {
+      process.stdout.write('.\n');
+    }, alarm * 1000);
+  });
